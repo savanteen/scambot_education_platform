45e000e0326695c9fede7da7b60711b96fa30f43
savanteen <dexter.tiah@gmail.com>
2025-11-16 22:40:36 +0800

    Update Registration.js

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -25,7 +25,7 @@ const Registration = ({ onRegister }) => {
     setError('');
     
     try {
-      const response = await axios.post('http://localhost:5000/api/users/register', {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/users/register`, {
         email,
         gameName
       });
